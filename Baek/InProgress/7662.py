b931592975a96bbaff8bcaa8f13ab7bd5314ae93
bluelemon61 <67902252+bluelemon61@users.noreply.github.com>
2023-03-13 00:22:35 +0900

    Update 7662.py

--- Baek/InProgress/7662.py
+++ Baek/InProgress/7662.py
@@ -13,13 +13,29 @@ def insertToMin(n):
     else:
         min_dict[n] = 1
     
+    index = len(min_tree)
+    while(index != 1):
+        parent = index % 2
+        min_tree.append(n)
+        if min_tree[parent] < n:
+            break
+        min_tree[parent], min_tree[index] = (min_tree[index], min_tree[parent])
+        index = parent
+    return index
+
+def delInMin():
+    result = min_tree[1]
+    min_tree[1] = min_tree.pop()
     index = 1
     while(True):
-        if len(min_tree) >= index: # Check the out of lange
-            min_tree.append(n)
-        else:
-            if min_tree[index] > n:
-                
+        toMin = []
+        child1 = index * 2
+        child2 = child1 + 1
+        if child1 >= len(min_tree):
+            
+
+
+    return result
 
 
 def insert(n):
